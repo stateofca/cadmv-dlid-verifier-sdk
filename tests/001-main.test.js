751e80f21a4f880aa7ab011992babaa932d60d8e
David I. Lehn <dlehn@digitalbazaar.com>
2026-02-27 17:14:13 -0500

    Update licenses.

--- tests/001-main.test.js
+++ tests/001-main.test.js
@@ -1,3 +1,9 @@
+/*!
+ * Copyright 2024 - 2026 California Department of Motor Vehicles.
+ * Copyright 2018 - 2026 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 import {describe, expect, it} from 'vitest';
 
 import {verify} from '../lib/index.js';
